7c44b39fd528e25395bcc1be7965b5ae05492eb2
Rubens Mariuzzo <rubens@mariuzzo.com>
2013-02-20 10:45:30 -0400

    NuGet package created with specification & some namespace renaming.

--- ASP.NET-MVC-Extras/Properties/AssemblyInfo.cs
+++ ASP.NET-MVC-Extras/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ASP.NET-MVC-Extras")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Extra and maybe missing components for ASP.NET MVC.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Mariuzzo")]
 [assembly: AssemblyProduct("ASP.NET-MVC-Extras")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2013")]
+[assembly: AssemblyCopyright("Copyright © Rubens Mariuzzo 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.*")]
+[assembly: AssemblyFileVersion("0.1.0.*")]
