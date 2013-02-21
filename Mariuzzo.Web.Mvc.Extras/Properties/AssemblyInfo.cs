1f7d5c4033cc0fbb8a72de1cab18f3a7184c90fe
Rubens Mariuzzo <rubens@mariuzzo.com>
2013-02-21 10:14:02 -0400

    Version number fixed.

--- Mariuzzo.Web.Mvc.Extras/Properties/AssemblyInfo.cs
+++ Mariuzzo.Web.Mvc.Extras/Properties/AssemblyInfo.cs
@@ -2,9 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Mariuzzo.Web.Mvc.Extras")]
 [assembly: AssemblyDescription("Extra missing components for ASP.NET MVC.")]
 [assembly: AssemblyConfiguration("")]
@@ -13,24 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © Rubens Mariuzzo 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("2eec8618-2b20-4dc1-bf37-6052e185058c")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.*")]
-[assembly: AssemblyFileVersion("0.1.0.*")]
+[assembly: AssemblyVersion("13.2.*")]
+[assembly: AssemblyFileVersion("13.2.*")]
